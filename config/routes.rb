f13700a9a99b78a987e3b3c9bc5ef0385a4e2715
prot3ct <djorkaevhell@abv.bg>
2017-01-28 23:44:49 +0200

    Init

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
 	root 'visits#index'
-	match '/visits',  to: 'visits#show', via: 'get'
-	match '/visits' => 'visits#create', via: :post
+	get '/visits',  to: 'visits#show'
+	post '/visits', to: 'visits#create'
 end
